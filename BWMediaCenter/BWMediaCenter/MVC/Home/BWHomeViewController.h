79b45343aa38d179b46cbbcd88cad696f06d241a
BobWong <wangbo@bluemoon.com.cn>
2017-05-18 14:37:09 +0800

    Modify

---
+++ BWMediaCenter/BWMediaCenter/MVC/Home/BWHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  BWHomeViewController.h
+//  BWiOSProjectFramework
+//
+//  Created by BobWong on 16/12/1.
+//  Copyright © 2016年 BobWongStudio. All rights reserved.
+//
+
+#import "BWBaseViewController.h"
+
+@interface BWHomeViewController : BWBaseViewController
+
+@end
