58cbdd0dc6bbafa1e9fc77e7937479374af7e3e7
BobWong <wangbo@bluemoon.com.cn>
2017-05-19 15:24:40 +0800

    Modify

---
+++ BWMediaCenter/BWMediaCenter/MVC/AudioPlayer/BWAudioPlayerViewController.h
@@ -0,0 +1,13 @@
+//
+//  BWAudioPlayerViewController.h
+//  BWMediaCenter
+//
+//  Created by BobWong on 2017/5/19.
+//  Copyright © 2017年 BobWongStudio. All rights reserved.
+//
+
+#import "BWBaseViewController.h"
+
+@interface BWAudioPlayerViewController : BWBaseViewController
+
+@end
---
+++ BWMediaCenter/BWMediaCenter/MVC/AudioPlayer/BWAudioPlayerViewController.m
@@ -0,0 +1,23 @@
+//
+//  BWAudioPlayerViewController.m
+//  BWMediaCenter
+//
+//  Created by BobWong on 2017/5/19.
+//  Copyright © 2017年 BobWongStudio. All rights reserved.
+//
+
+#import "BWAudioPlayerViewController.h"
+
+@interface BWAudioPlayerViewController ()
+
+@end
+
+@implementation BWAudioPlayerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+@end
